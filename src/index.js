df36f68b5bb4df876989d874b2e79b2623947cb7
Shikhaaa <vyasshikha96@gmail.com>
2021-05-27 09:54:27 +0530

    Refactor the code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import LoginComponent from './components/loginComponent/LoginComponent.js';
-
+//akdjlakjdask
 ReactDOM.render(
   <React.StrictMode>
     <App />
